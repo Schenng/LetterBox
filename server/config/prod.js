24dcfd232b59cabe721caa018dcea1664472e739
Schenng <scheng365@gmail.com>
2018-01-05 00:16:46 -0500

    Fix Heroku Error - (rename prod key)

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,13 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.CLIENT_SECRET,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
+}
+
+module.exports = {
+  googleClientID: '235435919264-9hpvg38pjde5gt0vmjs5vap3hvj0pm9v.apps.googleusercontent.com',
+  googleClientSecret: 'EBa7iS5xFiDS42KqRiZPT7uH',
+  mongoURI: 'mongodb://schenng:CB891E6433@ds139067.mlab.com:39067/letterbox-dev',
+  cookieKey: 'qweoyqoiweoqu'
 }
