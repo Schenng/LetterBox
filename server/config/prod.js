9e3a8d11695a89791695562c1862f9885248e407
Schenng <scheng365@gmail.com>
2018-01-05 00:20:41 -0500

    Remove reference keys

--- server/config/prod.js
+++ server/config/prod.js
@@ -4,10 +4,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 }
-
-module.exports = {
-  googleClientID: '235435919264-9hpvg38pjde5gt0vmjs5vap3hvj0pm9v.apps.googleusercontent.com',
-  googleClientSecret: 'EBa7iS5xFiDS42KqRiZPT7uH',
-  mongoURI: 'mongodb://schenng:CB891E6433@ds139067.mlab.com:39067/letterbox-dev',
-  cookieKey: 'qweoyqoiweoqu'
-}
