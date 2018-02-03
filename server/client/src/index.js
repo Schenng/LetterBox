d02611084800bda6f60fd65ee5a16a6242c7e96f
Schenng <scheng365@gmail.com>
2018-02-03 17:52:37 -0500

    React Router + Default routes

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,15 @@
+//This is for Redux Setup
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers'
 
+import App from './components/App';
 
-ReactDOM.render();
+const store = createStore(reducers , {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store ={store}>< App/></Provider>,
+   document.querySelector('#root'));
