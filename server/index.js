6caedad2f63379c55939dff8e9e5670ce4fbe288
Schenng <scheng365@gmail.com>
2018-02-10 15:27:06 -0500

    **** Allow assets to be served via epxress

--- server/index.js
+++ server/index.js
@@ -27,5 +27,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Serve production assets if they match
+  app.use(express.static('client/build'));
+
+  //Or else serve index.html file 
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
